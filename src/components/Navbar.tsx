0cc633b3bd4ab580975fefae2bec0d6c51aed9d8
Sawera <saweralangah347@gmail.com>
2025-01-02 14:12:22 -0800

    Created Header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,19 +26,6 @@ export default function Navbar() {
             </p>
           </div>
 
-          <div className="lg:hidden flex items-center">
-            <button onClick={toggleMenu} className="text-orange-800 hover:text-pink-800">
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
-              </svg>
-            </button>
-          </div>
         </div>
         </div>
       </div>
